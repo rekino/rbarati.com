6d9522f35880e1a769001a6075716f37ab78065a
Ramin Barati <rekino@gmail.com>
2025-06-03 22:35:28 +0200

    add a script for populating the pinecone index

---
+++ src/initPinecone.ts
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+import pinecone from './config/pinecone';
+import experiences from '../experiences.json';
+import AppConfig from "./appconfig";
+
+async function run() {
+    const existingIndexes = await pinecone.listIndexes();
+    const indexExists = existingIndexes.indexes?.some(index => {
+        return index.name === AppConfig.pinecone_index
+    });
+
+    if (!indexExists) {
+        console.error('Index does not exists. Creating the index...');
+        await pinecone.createIndexForModel({
+            name: AppConfig.pinecone_index,
+            cloud: 'aws',
+            region: 'us-east-1',
+            embed: {
+                model: 'multilingual-e5-large',
+                fieldMap: { text: 'chunk_text' },
+            },
+            waitUntilReady: true,
+        });
+    }
+
+    const index = pinecone.index(AppConfig.pinecone_index).namespace(AppConfig.pinecone_namespace);
+
+    const records = experiences.map(exp => {
+        return {
+            id: exp.id,
+            chunk_text: `${exp.role} at ${exp.company} - goals: ${exp.goal} - tasks: ${exp.tasks} - results: ${exp.results} - skills: ${exp.skills.join(',')}`,
+            role: exp.role,
+            company: exp.company,
+            goal: exp.goal,
+            tasks: exp.tasks,
+            results: exp.results,
+            skills: exp.skills
+        }
+    });
+
+    // Upload to Pinecone
+    await index.upsertRecords(records);
+
+    console.log(`Successfully upserted ${records.length} records to Pinecone.`);
+}
+
+run().catch((err) => {
+  console.error('Error populating Pinecone index:', err);
+  process.exit(1);
+});
