9316713b0dad236aad44ae6f10fd52781e74b58d
Ramin Barati <rekino@gmail.com>
2025-06-03 22:34:39 +0200

    update appconfig.ts with the new name of the index

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -1,7 +1,7 @@
 export default {
     together_llm_model: "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
 
-    pinecone_index: "questions",
+    pinecone_index: "experiences",
     pinecone_namespace: "default",
 
     db_host: "localhost",
